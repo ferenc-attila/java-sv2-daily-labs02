c531d1113388740a4b5923697011b0c92ca3e490
Attila <attila.ferenc.80@gmail.com>
2021-10-19 15:44:50 +0200

    modified values in day02.Geometry

--- week02-lab-tasks/src/main/java/day02/Geometry.java
+++ week02-lab-tasks/src/main/java/day02/Geometry.java
@@ -4,7 +4,7 @@ public class Geometry {
 
     public static void main(String[] args) {
 
-        Rectangle rectangle = new Rectangle(5.0,15.0);
+        Rectangle rectangle = new Rectangle(5.0,3.0);
 
         double area = rectangle.calculateArea();
 
